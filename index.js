483e0a60b9e430c1b50ff05b32ee9a9309ccdbb0
louysa <doganlouysa@outlook.com>
2026-02-02 21:21:56 +0300

    system cordinates

--- index.js
+++ index.js
@@ -9,18 +9,29 @@ const FOREGROUND = "#50FF50"
 const ctx = game.getContext("2d")
 
 console.log(ctx)
+
 function clear(){
     ctx.fillStyle = BACKGROUND;
     ctx.fillRect(0,0,game.width,game.height)
 }
 
-function point(x,y){
+function point({x,y}){
     const s = 20;
     ctx.fillStyle=FOREGROUND
-    ctx.fillRect(x,y,s,s);
+    ctx.fillRect(x - s/2,y- s/2,s,s);
 }
 
 
 clear();
-point(100,100);
+point(screen({x:-1,y:1}));
+
+
+function screen(p){
+    // -1...1  + 1 => 0..2  / 2 => 0..1 x Width => 0..w
+    return {
+        x: (p.x + 1) / 2 * game.width,
+        y: (1-(p.y + 1)/2) * game.height,
+    }
+
+}
 
